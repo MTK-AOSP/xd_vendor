a0c24300064a699f14026b5b439695561c0e552b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-09-02 13:33:23 +0000

    xdroid: port faceunlock feature
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -27,3 +27,13 @@ $(call inherit-product, vendor/xdUI/config.mk)
 # Inherit Google Packages
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
